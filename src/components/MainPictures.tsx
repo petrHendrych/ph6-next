2eb1f0c6ab1612c11b3af8689a51e3dbdc59cc3f
Petr Hendrych <phendrych@purple-technology.com>
2025-10-29 12:09:02 +0100

    feat: update dependencies and add GSAP packages for animations

--- src/components/MainPictures.tsx
+++ src/components/MainPictures.tsx
@@ -1,35 +1,78 @@
-import fs from 'fs';
-import path from 'path';
-
+'use client';
 import React from 'react';
 import Image from 'next/image';
+import { gsap } from 'gsap';
+import { TextPlugin } from 'gsap/TextPlugin';
+import { useGSAP } from '@gsap/react';
 
-const POST_FOLDER_NAME = 'main';
-const POSTS_DIRECTORY = path.join(process.cwd(), 'public', POST_FOLDER_NAME);
+import TweenTarget = gsap.TweenTarget;
 
-export const getPostLength = (): number =>
-	fs.readdirSync(POSTS_DIRECTORY).length;
+gsap.registerPlugin(useGSAP, TextPlugin);
+
+const next = 5; // time to change
+const fade = 1.5; // fade time
 
 const MainPictures = () => {
-	console.log(getPostLength());
+	useGSAP(() => {
+		const imgs: TweenTarget[] = gsap.utils.toArray('#imgContainer .motiv');
+
+		gsap.set(imgs[0], { autoAlpha: 1 });
+
+		const crossfade = () => {
+			gsap
+				.timeline()
+				.to(imgs[0], { autoAlpha: 0, duration: fade })
+				.to(imgs[1], { autoAlpha: 1, duration: fade }, 0);
+
+			imgs.push(imgs.shift() as TweenTarget);
+			gsap.delayedCall(next, crossfade);
+		};
+
+		gsap.delayedCall(next, crossfade);
+	});
+
 	return (
-		<>
-			{Array(getPostLength())
-				.fill(0)
-				.map((_, i) => (
-					<Image
-						className="absolute left-0 top-0 h-full w-full"
-						key={i}
-						src={`/main/uvod_${i + 1}.avif`}
-						alt="Main slideshow picture"
-						width={0}
-						height={0}
-						sizes="100vw"
-						style={{ width: '100%', height: 'auto' }}
-						priority
-					/>
-				))}
-		</>
+		<div
+			id="imgContainer"
+			className="relative h-screen w-full overflow-hidden pt-10"
+		>
+			<div className="motiv absolute left-0 top-0 h-screen w-full">
+				<Image
+					src="/main/uvod_1.avif"
+					alt="tmp"
+					width={0}
+					height={0}
+					sizes="100vw"
+					className="absolute"
+					priority
+					style={{ width: '100%', height: 'auto' }}
+				/>
+			</div>
+
+			<div className="motiv absolute left-0 top-0 h-screen w-full opacity-0">
+				<Image
+					src="/main/uvod_2.avif"
+					alt="tmp"
+					width={0}
+					height={0}
+					sizes="100vw"
+					className="absolute"
+					style={{ width: '100%', height: 'auto' }}
+				/>
+			</div>
+
+			<div className="motiv absolute left-0 top-0 h-screen w-full opacity-0">
+				<Image
+					src="/main/uvod_3.avif"
+					alt="tmp"
+					width={0}
+					height={0}
+					sizes="100vw"
+					className="absolute"
+					style={{ width: '100%', height: 'auto' }}
+				/>
+			</div>
+		</div>
 	);
 };
 
