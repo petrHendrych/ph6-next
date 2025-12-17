4a4ffa66d0cd952d2b2dff7be1b88be05ab24d12
Petr Hendrych <phendrych@purple-technology.com>
2025-12-17 20:48:54 +0100

    feat: add optional `className` to Image type and extend previewImages data

--- src/data.ts
+++ src/data.ts
@@ -22,51 +22,63 @@ export const mainImages: Image[] = [
 export const previewImages: Image[] = [
 	{
 		src: 'al_tit',
-		title: 'Title text for hover'
+		title: 'Title text for hover',
+		className: 'one'
 	},
 	{
 		src: 'alb_tit',
-		title: 'Title text for hover'
+		title: 'Title text for hover',
+		className: 'one'
 	},
 	{
 		src: 'aluvod',
-		title: 'Title text for hover'
+		title: 'Title text for hover',
+		className: 'two'
 	},
 	{
 		src: 'ambasada_tit',
-		title: 'Title text for hover'
+		title: 'Title text for hover',
+		className: 'three'
 	},
 	{
 		src: 'cer_tit',
-		title: 'Title text for hover'
+		title: 'Title text for hover',
+		className: 'two'
 	},
 	{
 		src: 'gfuvod',
-		title: 'Title text for hover'
+		title: 'Title text for hover',
+		className: 'one'
 	},
 	{
 		src: 'gr0',
-		title: 'Title text for hover'
+		title: 'Title text for hover',
+		className: 'two'
 	},
 	{
 		src: 'aluvod',
-		title: 'Title text for hover'
+		title: 'Title text for hover',
+		className: 'two'
 	},
 	{
 		src: 'ambasada_tit',
-		title: 'Title text for hover'
+		title: 'Title text for hover',
+		className: 'three'
 	},
 	{
 		src: 'cer_tit',
-		title: 'Title text for hover'
+		title: 'Title text for hover',
+		className: 'three'
 	},
 	{
 		src: 'gfuvod',
-		title: 'Title text for hover'
+		title: 'Title text for hover',
+		className: 'one'
 	},
 	{
 		src: 'gr0',
-		title: 'Title text for hover'
+		title: 'Title text for hover',
+		className: 'three'
 	}
 ];
 
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,7 @@
 export type Image = {
 	src: string;
 	title: string;
+	className?: string;
 };
 
 export type Reward = {
